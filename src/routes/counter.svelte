34510d1a60b91af63b41447284064356d72bcb03
Hanya You <youh@catlin.edu>
2026-03-04 22:05:23 -0800

    feat: counter

---
+++ src/routes/counter.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let { number, show }: { number: number, show: boolean } = $props();
+</script>
+
+{#if (show)}
+    <div style="display: flex; position: fixed; bottom: 0px; left: 0px;">
+        <img src="src/lib/images/counter.gif" height=80>
+        {#each [...(number.toString(10))] as char}
+            <img src="src/lib/images/{char}.gif" height=70>
+        {/each}
+    </div>
+{/if}
